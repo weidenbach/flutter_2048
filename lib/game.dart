2a288dd8412fb9526f37b8e1d5ada73efbc6fdc7
weidenbach <73471359+weidenbach@users.noreply.github.com>
2022-01-11 17:07:01 +0100

    Adds boardmanager for the game logic and animations for tiles. Still WIP.

--- lib/game.dart
+++ lib/game.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'tile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp],
-  ); // To turn off landscape mode
+  ); // Turns off landscape mode
   runApp(const App2048());
 }
 
@@ -66,7 +67,8 @@ class Board extends StatelessWidget {
                   ],
                 ),
               ),
-              BoardTiles(boardSize: boardSize, padding: padding, margin: margin)
+              BoardManager(
+                  boardSize: boardSize, padding: padding, margin: margin)
             ],
           ),
         ),
@@ -75,44 +77,169 @@ class Board extends StatelessWidget {
   }
 }
 
-class BoardTiles extends StatefulWidget {
-  const BoardTiles(
+class BoardManager extends StatefulWidget {
+  BoardManager(
       {Key? key,
       required this.boardSize,
       required this.padding,
       required this.margin})
-      : super(key: key);
-
+      : super(key: key) {
+    tileSize = ((boardSize - margin) - padding * 5) / 4;
+  }
+  late final double tileSize;
   final double boardSize;
   final double padding;
   final double margin;
+  List<Tile> tiles = [];
+
+  // Contains indexes of the tiles list.
+  List<int> boardState = List.filled(16, -1, growable: false);
 
   @override
-  _BoardTilesState createState() => _BoardTilesState();
+  _BoardManagerState createState() => _BoardManagerState();
 }
 
-class _BoardTilesState extends State<BoardTiles> {
+class _BoardManagerState extends State<BoardManager> {
+  final double _swipeSensitivity = 2;
+  bool _isAnimationOngoing = false;
+
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return GestureDetector(
-          onPanUpdate: (details) {
-            if (details.delta.dx > 0) {
-              print("rechts");
+    return Stack(children: [
+      LayoutBuilder(
+        builder: (context, constraints) {
+          return GestureDetector(
+            onPanUpdate: (details) {
+              if (details.delta.dx.abs() > _swipeSensitivity) {
+                handleSwipe(details.delta.dx, null);
+              } else if (details.delta.dy.abs() > _swipeSensitivity) {
+                handleSwipe(null, details.delta.dy);
+              }
+            },
+          );
+        },
+      ),
+      Stack(
+        children: widget.tiles,
+      )
+    ]);
+  }
+
+  Future<void> handleSwipe(double? xMovement, double? yMovement) async {
+    //  0  1  2  3
+    //  4  5  6  7
+    //  8  9  10 11
+    //  12 13 14 15
+    if (xMovement != null) {
+      //swiping horizontally
+      if (xMovement > 0) {
+        // swiping right
+        for (int i = 0; i < 16; i += 4) {
+          bool didCombine = false;
+          for (int j = 2; j >= 0; j--) {
+            for (int k = 1; k + j < 4; k++) {
+              int curTileIndex = widget.boardState[i + j];
+              if (curTileIndex == -1) {
+                continue;
+              }
+              if (widget.boardState[i + j + k] != -1) {
+                if (widget.tiles[curTileIndex].number ==
+                        widget.tiles[widget.boardState[i + j + k]].number &&
+                    !didCombine) {
+                  //TODO:
+                  var xyPos = _calculateBoardPositionFromGridNumber(i + j + k);
+                  widget.tiles[curTileIndex].globalKey.currentState
+                      ?.animateToPosition(xyPos[0], xyPos[1]);
+                  //TODO MERGE TILES
+                  didCombine = true;
+                  k = 4;
+                } else {
+                  if (k > 1) {
+                    var xyPos =
+                        _calculateBoardPositionFromGridNumber(i + j + k - 1);
+                    widget.tiles[curTileIndex].globalKey.currentState
+                        ?.animateToPosition(xyPos[0], xyPos[1]);
+                    break;
+                  }
+                }
+              } else if (j + k == 3) {
+                var xyPos = _calculateBoardPositionFromGridNumber(i + j + k);
+                widget.tiles[curTileIndex].globalKey.currentState
+                    ?.animateToPosition(xyPos[0], xyPos[1]);
+                widget.boardState[i + j + k] = widget.boardState[curTileIndex];
+                widget.boardState[curTileIndex] = -1;
+              }
             }
-            if (details.delta.dx < 0) {
-              print("links");
-            }
-            if (details.delta.dy < 0) {
-              print("hoch");
-            }
-            if (details.delta.dy > 0) {
-              print("runter");
-            }
-          },
-        );
-      },
-    );
+            // If tile to right
+            //    if canMerge
+            //        merge;
+            //        lockThatTileForThisMovement;
+            //    else
+            //        lookNextTile;
+
+            // Check if tile is to the right,
+            // if yes, check if can merge
+            //         otherwise move to position before that tile.
+            // if no, check next tile
+          }
+        }
+      } else {
+        //swiping left
+      }
+    } else {
+      //swiping vertically
+    }
+
+    if (!_isAnimationOngoing) {
+      _isAnimationOngoing = true;
+      _createTile();
+      await Future.delayed(const Duration(milliseconds: 800), () {
+        _isAnimationOngoing = false;
+      });
+    }
+  }
+
+  void _createTile() {
+    setState(() {
+      //TODO generate random position on board.
+      print("added Tile, tilesize: ${widget.tileSize}");
+      var key = GlobalKey<TileState>();
+      double number = 2;
+      if (!widget.tiles.isEmpty) {
+        number += 2;
+      }
+      widget.tiles.add(Tile(
+        key: key,
+        globalKey: key,
+        xPos: 7,
+        yPos: 7,
+        number: number,
+        tileSize: widget.tileSize,
+      ));
+      widget.boardState[0] = widget.tiles.length - 1;
+      key = GlobalKey<TileState>();
+      widget.tiles.add(Tile(
+        key: key,
+        globalKey: key,
+        xPos: 7,
+        yPos: 14 + widget.tileSize,
+        number: number,
+        tileSize: widget.tileSize,
+      ));
+      widget.boardState[4] = widget.tiles.length - 1;
+      // for (var w in widget.tiles) {
+      // w.globalKey.currentState?.animateToPosition(7, widget.tileSize + 14);
+      // }
+    });
+  }
+
+  List<double> _calculateBoardPositionFromGridNumber(int gridNumber) {
+    var xTileIdx = gridNumber % 4;
+    var yTileIdx = gridNumber ~/ 4;
+    double xPos =
+        widget.padding + (widget.tileSize + widget.padding) * xTileIdx;
+    double yPos =
+        widget.padding + (widget.tileSize + widget.padding) * yTileIdx;
+    return [xPos, yPos];
   }
 }
--- lib/tile.dart
+++ lib/tile.dart
@@ -1,7 +1,88 @@
-class Tile {
-  int xPos;
-  int yPos;
-  int number;
+import 'package:flutter/material.dart';
 
-  Tile(this.xPos, this.yPos, this.number);
+class Tile extends StatefulWidget {
+  double xPos;
+  double yPos;
+  double number;
+  final double tileSize;
+
+  final GlobalKey<TileState> globalKey;
+
+  Tile({
+    Key? key,
+    required this.globalKey,
+    required this.xPos,
+    required this.yPos,
+    required this.number,
+    required this.tileSize,
+  }) : super(key: key);
+
+  void animateToPosition(double newXPos, double newYPos) {}
+
+  @override
+  TileState createState() => TileState();
+}
+
+class TileState extends State<Tile> with SingleTickerProviderStateMixin {
+  double _newXPos = 250;
+  double _newYPos = 250;
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 400),
+    vsync: this,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final Size biggest = constraints.biggest;
+        return Stack(
+          children: <Widget>[
+            RelativePositionedTransition(
+              size: biggest,
+              rect: RectTween(
+                begin: Rect.fromLTWH(
+                    widget.xPos, widget.yPos, widget.tileSize, widget.tileSize),
+                end: Rect.fromLTWH(
+                    _newXPos, _newYPos, widget.tileSize, widget.tileSize),
+              ).animate(CurvedAnimation(
+                parent: _controller,
+                curve: Curves.ease,
+              )),
+              child: Card(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${widget.number}',
+                        textScaleFactor: 3,
+                        textAlign: TextAlign.center,
+                      )
+                    ]),
+                margin: EdgeInsets.all(0),
+                borderOnForeground: false,
+                shadowColor: Colors.white,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void animateToPosition(double newXPos, double newYPos) {
+    print("anitmatetoPostioin");
+    setState(() {
+      _newXPos = newXPos;
+      _newYPos = newYPos;
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 }
